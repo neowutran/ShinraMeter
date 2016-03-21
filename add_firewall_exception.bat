36946144d0dacc07bcfa2e8560fa00e4e3ee552c
neowutran <me@neowutran.net>
2016-03-21 23:54:08 +0100

    Update add_firewall_exception.bat

--- add_firewall_exception.bat
+++ add_firewall_exception.bat
@@ -1,21 +1,13 @@
 @echo off
 
-goto check_Permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	for %%x in (in out) do (
+		netsh advfirewall firewall add rule action=allow profile=any protocol=any enable=yes direction=%%x name=ShinraMeter program = "%~dp0%ShinraMeter.exe" > NUL
+	)
+	echo Rule added to the firewall
+) else (
+	echo You must run this program in admin mode 
+)
 
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        for %%x in (in out) do (
-			netsh advfirewall firewall add rule action=allow profile=any protocol=any enable=yes direction=%%x name=ShinraMeter program = "%~dp0%ShinraMeter.exe" > NUL
-		)
-		set /p delBuild=Rule added to the firewall: 
-    ) else (
-		set /p delBuild=You must run this program in admin mode: 
-    )
-
-    pause >nul
-	
-	
-
+pause >nul
