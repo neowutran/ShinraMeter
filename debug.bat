0e671b680aa71b0a77ac4dca8bde41eb97fce272
Gl0 <ab@glk.spb.ru>
2016-05-18 20:54:22 +0300

    Fix excel export (again=)

--- debug.bat
+++ debug.bat
@@ -1,12 +1,13 @@
 @echo off
-msbuild Tera.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild Tera.sln /p:Configuration=Debug /p:Platform="Any CPU"
 set output=.\ShinraMeterV
 set source=.
-set variant=Release
+set variant=Debug
 rmdir /Q /S "%output%"
 md "%output%
 md "%output%\resources"
 md "%output%\resources\config"
+
 copy "%source%\DamageMeter.Sniffing\bin\%variant%\*" "%output%\"
 copy "%source%\DamageMeter.UI\bin\%variant%\*" "%output%\"
 copy "%source%\Randomizer\bin\%variant%\Randomizer.exe" "%output%\"
@@ -16,6 +17,5 @@ copy "%source%\add_firewall_exception.bat" "%output%\add_firewall_exception.bat"
 xcopy "%source%\resources" "%output%\resources\" /E
 del "%output%\*.xml"
 del "%output%\*.vshost*"
-del "%output%\*.pdb"
 del "%output%\resources\logs\*"
 del "%output%\resources\config\*.xml"
