b38e5c262537df49e7057052e6afb2e2ca8af2f2
Gl0 <ab@glk.spb.ru>
2016-06-06 04:07:37 +0300

    fix partyonly setting was not saved

--- Data/WindowData.cs
+++ Data/WindowData.cs
@@ -404,6 +404,7 @@ namespace Data
             xml.Root.Add(new XElement("excel_save_directory", ExcelSaveDirectory));
             xml.Root.Add(new XElement("always_visible", AlwaysVisible));
             xml.Root.Add(new XElement("scale", Scale));
+            xml.Root.Add(new XElement("partyonly", Scale));
             xml.Root.Add(new XElement("showhealcrit", ShowHealCrit));
 
             _filestream.SetLength(0);
