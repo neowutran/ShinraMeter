62bfa18f3a31f2f39a0a3175de3f52b2f7c06ee7
Gl0 <ab@glk.spb.ru>
2016-06-18 22:52:01 +0300

    Fix JP encription, repack icon

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -16,7 +16,7 @@ namespace DamageMeter.AutoUpdate
 {
     public class UpdateManager
     {
-        public static readonly string Version = "1.05";
+        public static readonly string Version = "1.08";
             
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
