a9830770b0c9271ccf574a054568c0df3bb14f48
Gl0 <ab@glk.spb.ru>
2016-06-22 18:04:13 +0300

    Fix S_GET_USER_GUILD_LOGO error

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -16,7 +16,7 @@ namespace DamageMeter.AutoUpdate
 {
     public class UpdateManager
     {
-        public static readonly string Version = "1.08";
+        public static readonly string Version = "1.10";
             
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
