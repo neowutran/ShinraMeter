33496991e56963e9b33c7385dc0b38eee9a37768
neowutran <me@neowutran.ovh>
2016-07-07 13:51:30 +0200

    thanks to Gl0:

--- DamageMeter.Core/NetworkController.cs
+++ DamageMeter.Core/NetworkController.cs
@@ -131,6 +131,7 @@ namespace DamageMeter
             if (packetsWaiting > 1500 && filteredEntities.Count > 1)
             {
                 Database.Database.Instance.DeleteAllWhenTimeBelow(Encounter);
+                entities = Database.Database.Instance.AllEntity();
                 filteredEntities = entities.Select(entityid => EntityTracker.GetOrNull(entityid)).OfType<NpcEntity>().Where(npc => npc.Info.Boss).ToList();
             }
 
