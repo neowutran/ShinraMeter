91db91890aa3e41df74580ced5b0911f6dcb0501
Gl0 <ab@glk.spb.ru>
2016-08-01 14:10:32 +0300

    Fix missing buffs if buff include hp/mp regen Add SCHM to site export

--- DamageMeter.Core/DataExporter.cs
+++ DamageMeter.Core/DataExporter.cs
@@ -232,6 +232,7 @@ namespace DamageMeter
                 areaId != 468 && 
                 areaId != 770 && 
                 areaId != 769 &&
+                areaId != 916 &&
                 areaId != 969 && 
                 areaId != 970
                 )
