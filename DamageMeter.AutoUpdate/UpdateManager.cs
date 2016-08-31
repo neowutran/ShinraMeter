0314663cb4fce051de0acb15d52e65dd63a955d8
Gl0 <ab@glk.spb.ru>
2016-08-31 15:47:49 +0300

    1.27 JP server merge Update hotdot descriptions

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -16,7 +16,7 @@ namespace DamageMeter.AutoUpdate
 {
     public class UpdateManager
     {
-        public static readonly string Version = "1.26";
+        public static readonly string Version = "1.27";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
