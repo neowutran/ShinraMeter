372d5542634e91c13223b8a12862aafdda6ec5dd
neowutran <me@neowutran.ovh>
2016-09-30 17:29:05 +0200

    1.48

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     public class UpdateManager
     {
 
-        public static readonly string Version = "1.47";
+        public static readonly string Version = "1.48";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
