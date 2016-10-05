274e99525abb5cbfc9488412a84d62dac5cc8241
radasuka <anakin4122@gmail.com>
2016-10-05 21:49:29 +0900

    fix discord text

--- Data/WindowData.cs
+++ Data/WindowData.cs
@@ -325,8 +325,8 @@ namespace Data
 
                 string guildInfosText = ":dart: {guild_guildname}  :dart:\n\n{guild_master} - {guild_size}\n{gold_label}: {guild_gold}\n{xp_label} for next level: {guild_xp_to_next_level}\nCreation time: {guild_creationtime}\nQuest done status: {guild_number_quest_done}/{guild_total_number_quest}\n";
                 string questInfoText = ":dart: {quest_guildname} - {quest_type} - {quest_size} :dart:\n\nTime remaining: {quest_time_remaining}\nIs bam quest: {quest_is_bam_quest}\n{targets}\n{rewards}\n{quest_list}\n";
-                string questListInfoText = "QuestSize: {quest_size}\nTime remaining: {quest_time_remaining}\nIs bam quest: {quest_is_bam_quest}\n{targets}\n{rewards}\n";
-                string questListHeaderText = "----NoActive----\n";
+                string questListInfoText = "QuestSize: {quest_size}\nIs bam quest: {quest_is_bam_quest}\n{targets}\n{rewards}\n";
+                string questListHeaderText = "----NoActive----\n\n";
                 string rewardFooterText = "";
                 string rewardContentText = "{reward_name}: {reward_amount}\n";
                 string rewardHeaderText = "---------\n";
@@ -334,7 +334,7 @@ namespace Data
                 string targetHeaderText = "---------\n";
                 string targetContentText = "{target_name}: {target_current_count}/{target_total_count}\n";
                 string targetFooterText = "";
-                string questNoActiveText = ":dart:   {guild_guildname}   :dart:\n\n{no_quest_text}\n{quest_list}\n\n";
+                string questNoActiveText = ":dart:   {guild_guildname}   :dart:\n\n{no_quest_text}\n\n{quest_list}\n";
 
                 var guildInfosTextElement = guild.Element("guild_infos_text");
                 if (guildInfosTextElement != null) guildInfosText = guildInfosTextElement.Value;
