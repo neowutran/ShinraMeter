3c59f02a3e7d146cb379821509e2c9540fdb57da
Gl0 <ab@glk.spb.ru>
2016-10-24 02:11:58 +0300

    Site upload button will work only if upload error for the current boss found remove some debug

--- DamageMeter.Core/Processing/C_CHECK_VERSION.cs
+++ DamageMeter.Core/Processing/C_CHECK_VERSION.cs
@@ -18,13 +18,13 @@ namespace DamageMeter.Processing
         internal C_CHECK_VERSION(Tera.Game.Messages.C_CHECK_VERSION message)
         {
             Debug.WriteLine("VERSION0 = " + message.Versions[0]);
-            Debug.WriteLine("VERSION1 = " + message.Versions[1]);
+//            Debug.WriteLine("VERSION1 = " + message.Versions[1]);
             var opCodeNamer =
                 new OpCodeNamer(Path.Combine(BasicTeraData.Instance.ResourceDirectory,
                     $"data/opcodes/{message.Versions[0]}.txt"));
             NetworkController.Instance.MessageFactory = new MessageFactory(opCodeNamer, NetworkController.Instance.Server.Region);
-            if (TeraSniffer.Instance.ClientProxyOverhead + TeraSniffer.Instance.ServerProxyOverhead > 0x100)
-                BasicTeraData.LogError("Client Proxy overhead: " + TeraSniffer.Instance.ClientProxyOverhead + "\r\nServer Proxy overhead: " + TeraSniffer.Instance.ServerProxyOverhead);
+            //if (TeraSniffer.Instance.ClientProxyOverhead + TeraSniffer.Instance.ServerProxyOverhead > 0x100)
+            //    BasicTeraData.LogError("Client Proxy overhead: " + TeraSniffer.Instance.ClientProxyOverhead + "\r\nServer Proxy overhead: " + TeraSniffer.Instance.ServerProxyOverhead);
         }
 
     }
