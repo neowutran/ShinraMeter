589824b9d6bc5b60f2725cd965c46970a33d13b3
Gl0 <ab@glk.spb.ru>
2016-11-16 16:21:18 +0300

    update batch files

--- debug.bat
+++ debug.bat
@@ -15,11 +15,9 @@ copy "%source%\Randomizer\bin\%variant%\Randomizer.exe" "%output%\"
 copy "%source%\Randomizer\bin\%variant%\Randomizer.exe.config" "%output%\"
 copy "%source%\ReadmeUser.txt" "%output%\readme.txt"
 copy "%source%\add_firewall_exception.bat" "%output%\add_firewall_exception.bat"
-xcopy "%source%\resources" "%output%\resources\" /E
+xcopy "%source%\resources" "%output%\resources\" /E /EXCLUDE:.\exclude.txt
 del "%output%\*.xml"
 del "%output%\error.log"
 del "%output%\*.vshost*"
-del "%output%\resources\config\*.xml"
-del "%output%\resources\config\*.txt"
 del "%output%\resources\data\hotdot\glyph*.tsv"
 del "%output%\resources\data\hotdot\abnormal.tsv"
