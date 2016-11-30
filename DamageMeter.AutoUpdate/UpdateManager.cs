dafc300abf335c40f144415c27a999c68409823e
Gl0 <ab@glk.spb.ru>
2016-11-30 19:20:42 +0300

    1.63 NA update Add Adrenaline Rush to default events (close #255)

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     public class UpdateManager
     {
 
-        public static readonly string Version = "1.62";
+        public static readonly string Version = "1.63";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
