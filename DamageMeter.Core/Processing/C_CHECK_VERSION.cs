37bde17e6e0f7a39e0d3f4417d9e99663fef6729
Gl0 <ab@glk.spb.ru>
2016-12-20 01:53:07 +0300

    Add SysMsgNamer

--- DamageMeter.Core/Processing/C_CHECK_VERSION.cs
+++ DamageMeter.Core/Processing/C_CHECK_VERSION.cs
@@ -22,7 +22,13 @@ namespace DamageMeter.Processing
             var opCodeNamer =
                 new OpCodeNamer(Path.Combine(BasicTeraData.Instance.ResourceDirectory,
                     $"data/opcodes/{message.Versions[0]}.txt"));
-            NetworkController.Instance.MessageFactory = new MessageFactory(opCodeNamer, NetworkController.Instance.Server.Region);
+            OpCodeNamer sysMsgNamer=null;
+            try //we we can have working opcodes from older KR versions included, but don't have sysmsg codes for them, todo: delete trycatch when we have all files for all upcoming versions.
+            {sysMsgNamer =
+                    new OpCodeNamer(Path.Combine(BasicTeraData.Instance.ResourceDirectory,
+                        $"data/opcodes/smt_{message.Versions[0]}.txt"));}
+            catch{}
+            NetworkController.Instance.MessageFactory = new MessageFactory(opCodeNamer, NetworkController.Instance.Server.Region, false, sysMsgNamer);
             if (TeraSniffer.Instance.ClientProxyOverhead + TeraSniffer.Instance.ServerProxyOverhead > 0x1000)
                 BasicTeraData.LogError("Client Proxy overhead: " + TeraSniffer.Instance.ClientProxyOverhead + "\r\nServer Proxy overhead: " + TeraSniffer.Instance.ServerProxyOverhead);
         }
