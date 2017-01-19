680bf86475d19dfd90c822371a8089307e43dc2a
Gl0 <ab@glk.spb.ru>
2017-01-19 16:59:48 +0300

    1.72 KR EU TW Updates

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     public class UpdateManager
     {
 
-        public static readonly string Version = "1.71";
+        public static readonly string Version = "1.72";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
