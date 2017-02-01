b0e847e77be3e9c368182630a69c4e8aebaec8c9
Gl0 <ab@glk.spb.ru>
2017-02-02 01:24:54 +0300

    1.75 JP update

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     public class UpdateManager
     {
 
-        public static readonly string Version = "1.74";
+        public static readonly string Version = "1.75";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
