76c393ab3c5272c709e68758db69d6a1bf504b6c
Gl0 <Gl0@users.noreply.github.com>
2017-03-07 19:05:57 +0300

    fix missing shinralauncher in full zip after db update

--- updateDB.bat
+++ updateDB.bat
@@ -6,6 +6,7 @@ set variant=Release
 md "%output%\resources"
 md "%output%\resources\config"
 xcopy "%source%\resources" "%output%\resources\" /E /EXCLUDE:.\exclude.txt
+copy "%source%\ShinraLauncher.exe" "%output%\"
 copy "%source%\.git\modules\resources\data\refs\heads\master" "%output%\resources\head"
 del "%output%\resources\data\hotdot\glyph*.tsv"
 del "%output%\resources\data\hotdot\abnormal.tsv"
