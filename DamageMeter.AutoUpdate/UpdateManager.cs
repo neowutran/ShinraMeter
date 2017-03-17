9386bad43aa2140f0db9a7f7e840bef47d6edb31
Gl0 <Gl0@users.noreply.github.com>
2017-03-18 01:51:45 +0300

    1.83

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -20,7 +20,7 @@ namespace DamageMeter.AutoUpdate
 
         private static Dictionary<string, string> _hashes;
         private static Dictionary<string, string> _latest;
-        public static string Version = "1.82";
+        public static string Version = "1.83";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
