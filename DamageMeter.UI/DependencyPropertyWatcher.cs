1b1d705e75d27362daaaf6c9c6d134f47ba9dc7c
foglio <foglio1024@gmail.com>
2017-05-04 13:11:01 +0200

    added DependencyPropertyWatcher utility for GenericGauge bindings and animations

---
+++ DamageMeter.UI/DependencyPropertyWatcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DamageMeter.UI
+{
+    public class DependencyPropertyWatcher<T> : DependencyObject, IDisposable
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value",
+                typeof(object),
+                typeof(DependencyPropertyWatcher<T>),
+                new PropertyMetadata(null, OnPropertyChanged));
+
+        public event EventHandler PropertyChanged;
+
+        public DependencyPropertyWatcher(DependencyObject target, string propertyPath)
+        {
+            this.Target = target;
+            BindingOperations.SetBinding(
+                this,
+                ValueProperty,
+                new Binding() { Source = target, Path = new PropertyPath(propertyPath), Mode = BindingMode.OneWay });
+        }
+
+        public DependencyObject Target { get; private set; }
+
+        public T Value
+        {
+            get { return (T)this.GetValue(ValueProperty); }
+        }
+
+        public static void OnPropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            DependencyPropertyWatcher<T> source = (DependencyPropertyWatcher<T>)sender;
+
+            if (source.PropertyChanged != null)
+            {
+                source.PropertyChanged(source, EventArgs.Empty);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.ClearValue(ValueProperty);
+        }
+    }
+
+}
