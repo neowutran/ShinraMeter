5f1404083c0f98291364d09dc1425a2cf134f21c
foglio <foglio1024@gmail.com>
2017-06-04 20:46:01 +0200

    layout and animation changes

--- DamageMeter.UI/PopupNotification.xaml.cs
+++ DamageMeter.UI/PopupNotification.xaml.cs
@@ -6,6 +6,9 @@ using System.Windows.Input;
 using System.Windows.Interop;
 using Data;
 using Data.Actions.Notify;
+using System.Windows.Media.Animation;
+using System;
+using System.Windows.Media;
 
 namespace DamageMeter.UI
 {
@@ -24,7 +27,7 @@ namespace DamageMeter.UI
             if (flash == null) { return; }
             if (flash.Balloon != null && flash.Balloon.DisplayTime >= 500)
             {
-                Value(flash.Balloon.TitleText, flash.Balloon.BodyText);
+                Value(flash.Balloon.TitleText, flash.Balloon.BodyText, flash.Balloon.EventType);
                 Display(flash.Balloon.DisplayTime);
             }
             if (!BasicTeraData.Instance.WindowData.MuteSound && flash.Sound != null) { Task.Run(() => flash.Sound.Play()); }
@@ -37,16 +40,39 @@ namespace DamageMeter.UI
             Topmost = false;
             Topmost = true;
             ShowWindow();
+            var w = root.ActualWidth;
+            root.BeginAnimation(WidthProperty, new DoubleAnimation(0, w, TimeSpan.FromMilliseconds(400)) { EasingFunction = new QuadraticEase() });
             _stupidNotSafeLock++;
             await Task.Delay(displayTime);
             _stupidNotSafeLock--;
             if (_stupidNotSafeLock == 0) { HideWindow(); }
         }
 
-        private void Value(string title, string text)
+        private void Value(string title, string text, EventType t)
         {
             TitleLabel.Content = title;
             TextBlock.Text = text;
+            Color col;
+            switch (t)
+            {
+                case EventType.MissingAb:
+                    col = Colors.Red;
+                    break;
+                case EventType.AddRemoveAb:
+                    col = Colors.Orange;
+                    break;
+                case EventType.Cooldown:
+                    col = Color.FromRgb(0xfd, 0x39, 0x20);
+                    break;
+                case EventType.AFK:
+                    col = Color.FromRgb(0x2, 0xbb, 0xff);
+                    break;
+                default:
+                    col = Color.FromRgb(0x2,0xbb,0xff);
+                    break;
+            }
+
+            rect.Fill = new SolidColorBrush(col);
         }
     }
 }
