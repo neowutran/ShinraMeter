ef1ac6dd09dc9d64d0e9abe7713ae7c490e34430
Gl0 <Gl0@users.noreply.github.com>
2017-09-08 15:05:30 +0300

    2.11 fix KR, show verbose error on unknown client version

--- DamageMeter.Core/Processing/C_CHECK_VERSION.cs
+++ DamageMeter.Core/Processing/C_CHECK_VERSION.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Windows;
 using DamageMeter.Sniffing;
 using Data;
+using Lang;
 using Tera.Game;
 
 namespace DamageMeter.Processing
@@ -11,12 +13,18 @@ namespace DamageMeter.Processing
         internal C_CHECK_VERSION(Tera.Game.Messages.C_CHECK_VERSION message)
         {
             Debug.WriteLine("VERSION0 = " + message.Versions[0]);
-//            Debug.WriteLine("VERSION1 = " + message.Versions[1]);
+            //            Debug.WriteLine("VERSION1 = " + message.Versions[1]);
+            if (!File.Exists(Path.Combine(BasicTeraData.Instance.ResourceDirectory, $"data/opcodes/{message.Versions[0]}.txt")))
+            {
+                BasicTeraData.LogError("Unknown client version: " + message.Versions[0]);
+                MessageBox.Show(LP.Unknown_client_version + message.Versions[0]);
+                NetworkController.Instance.Exit();
+            }
             var opCodeNamer = new OpCodeNamer(Path.Combine(BasicTeraData.Instance.ResourceDirectory, $"data/opcodes/{message.Versions[0]}.txt"));
             var sysMsgNamer = new OpCodeNamer(Path.Combine(BasicTeraData.Instance.ResourceDirectory, $"data/opcodes/smt_{message.Versions[0]}.txt"));
             TeraSniffer.Instance.Connected = true;
-            NetworkController.Instance.MessageFactory = new MessageFactory(opCodeNamer, NetworkController.Instance.Server.Region, message.Versions[0], false,
-                sysMsgNamer);
+            NetworkController.Instance.MessageFactory = new MessageFactory(opCodeNamer, NetworkController.Instance.Server.Region, message.Versions[0], false, sysMsgNamer);
+
             if (TeraSniffer.Instance.ClientProxyOverhead + TeraSniffer.Instance.ServerProxyOverhead > 0x1000)
             {
                 BasicTeraData.LogError("Client Proxy overhead: " + TeraSniffer.Instance.ClientProxyOverhead + "\r\nServer Proxy overhead: " +
