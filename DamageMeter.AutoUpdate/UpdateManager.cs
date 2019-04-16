028e814604c4fa85e21ae60c6ee880f69e55282b
Gl0 <Gl0@users.noreply.github.com>
2019-04-16 16:08:48 +0300

    2.66 fix crash on RU/EU after unplanned maintenance

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     {
         private static Dictionary<string, string> _hashes;
         private static Dictionary<string, string> _latest;
-        public static string Version = "2.65";
+        public static string Version = "2.66";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
