4de142e06af67150bb5425bd9166ae93017b902d
Gl0 <Gl0@users.noreply.github.com>
2021-06-30 14:08:12 +0300

    2.88 Patch 107

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     {
         private static Dictionary<string, string> _hashes;
         private static Dictionary<string, string> _latest;
-        public static string Version = "2.87";
+        public static string Version = "2.88";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
